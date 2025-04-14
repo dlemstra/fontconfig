63a050a03fb0574d2d4f5be7f8a5045007b47d1b
Dirk Lemstra <dirk@lemstra.org>
2025-04-14 17:06:26 +0200

    Updated fontconfig to 2.16.2

--- fc-fontations/mod.rs
+++ fc-fontations/mod.rs
@@ -1,6 +1,35 @@
+/*
+ * fontconfig/fc-fontations/mod.rs
+ *
+ * Copyright 2025 Google LLC.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of the author(s) not be used in
+ * advertising or publicity pertaining to distribution of the software without
+ * specific, written prior permission.  The authors make no
+ * representations about the suitability of this software for any purpose.  It
+ * is provided "as is" without express or implied warranty.
+ *
+ * THE AUTHOR(S) DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
 extern crate fc_fontations_bindgen;
 
-use fc_fontations_bindgen::{fcint::FcPatternCreate, FcFontSet, FcFontSetAdd};
+mod pattern_bindings;
+
+use fc_fontations_bindgen::{
+    fcint::{FcPatternCreate, FcPatternObjectAddBool, FC_COLOR_OBJECT},
+    FcFontSet, FcFontSetAdd,
+};
 
 #[no_mangle]
 /// Externally called in fcfontations.c as the file scanner function
@@ -15,6 +44,9 @@ pub unsafe extern "C" fn add_patterns_to_fontset(
     font_set: *mut FcFontSet,
 ) -> libc::c_int {
     let empty_pattern = FcPatternCreate();
+    // Test call to ensure that an FcPrivate API function FcPatternObjectAddBool
+    // is accessible and can be linked to.
+    FcPatternObjectAddBool(empty_pattern, FC_COLOR_OBJECT as i32, 0_i32);
     if !font_set.is_null() {
         FcFontSetAdd(
             font_set,
